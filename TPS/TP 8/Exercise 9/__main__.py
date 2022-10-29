c9a25cb17b7dfd286c44c94c30377170b2f67b32
David Monsalve <davidmonsalve717@gmail.com>
2022-10-29 01:13:15 -0300

    ex 7 tp 8

---
+++ TPS/TP 8/Exercise 9/__main__.py
@@ -0,0 +1,16 @@
+def leer(dic, n):
+    numero = str(n)
+    for name,value in dic.items():
+        print(numero + " x " + str(name) + " = " + str(value))
+
+def cargar(dic, n):
+    for i in range(12):
+        clave = i+1
+        dato = n * clave
+
+        dic[clave] = dato
+    leer(dic, n)
+
+dic = {}
+n = int(input("numero: "))
+dic = cargar(dic, n)
