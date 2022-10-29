304e78ebc354611858e3ae4c12eec65bb41ce9a2
David Monsalve <davidmonsalve717@gmail.com>
2022-10-29 01:32:52 -0300

    ex 10 tp 8

---
+++ TPS/TP 8/Exercise 10/__main__.py
@@ -0,0 +1,10 @@
+def eliminarclaves(lista, dic):
+    for i in range(len(lista)):
+        del dic[lista[i]]
+
+    print(dic)
+
+
+lista_claves = [5, 10, 30]
+dic = {5: "futbol", 8: "morgan freeman", 10:"gotham", 30:"uade"}
+eliminarclaves(lista_claves, dic)
